fa345dfd99da3529e5fc2eebb6e221fa3dbc4282
LatifahIbrahim <ilatifat183@gmail.com>
2025-10-17 06:32:52 +0100

    added a file

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin
  awk $3 =="'gene' {print #0}" sample_data.giff > gene.ffg
 
- awk $3 =="'gene' {print #0}" sample_data.giff > gene.ffg
+ awk $3 =="'exon' {print #0}" sample_data.giff > exon.ffg
